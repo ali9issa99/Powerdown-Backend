e784adaff99d3ceaeb7b90c26040f653fe1a94f6
ALI9ISSA9 <ali7333170@gmail.com>
2024-08-23 23:12:12 +0300

    created the 'getuser' operation to get users by id

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -16,3 +16,16 @@ export const createUser = async (req, res) => {
       res.status(500).json({ error: error.message });
     }
   };
+
+
+export const getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ message: 'User not found' });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
