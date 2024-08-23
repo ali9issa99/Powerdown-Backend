9eccc0f5cb9621e21d9b87c4c5787d50340f95ae
ALI9ISSA9 <ali7333170@gmail.com>
2024-08-24 02:16:51 +0300

    created routes in index.js for the user

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import usersRoutes from "./routes/usersRoutes.js";
 import connectToDatabase from "./database/connection.js";
 
 
@@ -7,6 +8,8 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+
+app.use("/users", usersRoutes);
 
 app.listen(8080, () => {
   console.log("Server running on port 8080");
