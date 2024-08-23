3ff0c1e3a38516b5126513d76949b187a46484c0
ALI9ISSA9 <ali7333170@gmail.com>
2024-08-23 23:06:11 +0300

    created the 'create' operation for the user

---
+++ src/controllers/usersController.js
@@ -0,0 +1,18 @@
+import { User } from "../models/userModel";
+
+
+export const createUser = async (req, res) => {
+    try {
+      const { name, email, password } = req.body;
+      const newUser = new User({
+        name,
+        email,
+        password,
+      });
+  
+      await newUser.save();
+      res.status(201).json(newUser);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
