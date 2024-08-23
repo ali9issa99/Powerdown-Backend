18c0dafd2649a458e7640442e36870dbfa3d4203
ALI9ISSA9 <ali7333170@gmail.com>
2024-08-23 23:47:04 +0300

    created the 'getallusers' operation for the users

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -29,3 +29,12 @@ export const getUser = async (req, res) => {
 };
 
 
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
