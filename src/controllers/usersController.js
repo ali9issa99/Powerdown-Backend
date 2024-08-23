109f615e7efccf8c12d955c1d21f66312a001248
ALI9ISSA9 <ali7333170@gmail.com>
2024-08-24 01:41:09 +0300

    created the 'deleteUser' operation for the user

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -59,3 +59,13 @@ export const updateUser = async (req, res) => {
   }
 };
 
+
+export const deleteUser = async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    if (!deletedUser) return res.status(404).json({ message: 'User not found' });
+    res.status(200).json({ message: 'User deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
