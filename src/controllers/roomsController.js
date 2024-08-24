fcaaf84c62822eb6a20d63bb6bf436ac1be51921
ALI9ISSA9 <ali7333170@gmail.com>
2024-08-24 17:30:26 +0300

    created the 'getAllRoom' operation for the Rooms collection

--- src/controllers/roomsController.js
+++ src/controllers/roomsController.js
@@ -26,3 +26,16 @@ export const getRoom = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+
+export const getAllRooms = async (req, res) => {
+    try {
+      const rooms = await Room.find({});
+      res.status(200).json(rooms);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
+
+
+  
