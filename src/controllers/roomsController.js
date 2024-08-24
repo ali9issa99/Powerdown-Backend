da8f258687f39069224e59b000d8562e99489e15
ALI9ISSA9 <ali7333170@gmail.com>
2024-08-24 17:31:34 +0300

    created the 'deleteRoom' operation for the Rooms collection

--- src/controllers/roomsController.js
+++ src/controllers/roomsController.js
@@ -56,3 +56,14 @@ export const getAllRooms = async (req, res) => {
       res.status(500).json({ error: error.message });
     }
   };
+
+
+  export const deleteRoom = async (req, res) => {
+    try {
+      const deletedRoom = await Room.findByIdAndDelete(req.params.id);
+      if (!deletedRoom) return res.status(404).json({ message: 'Room not found' });
+      res.status(200).json({ message: 'Room deleted successfully' });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
