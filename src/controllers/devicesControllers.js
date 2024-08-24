3c8c358a4122208326be06cdd32d8242fa2ecdb1
ALI9ISSA9 <ali7333170@gmail.com>
2024-08-24 14:24:55 +0300

    created 'getDevice' operation for the devices collection to get device by id

--- src/controllers/devicesControllers.js
+++ src/controllers/devicesControllers.js
@@ -9,10 +9,21 @@ export const createDevice = async (req, res) => {
                 room_id,
                 status,
             });
-            
+
             await newDevice.save();
             res.status(201).json(newDevice);
         }catch(error){
             res.status(500).json({error:error.message});
         }
 };
+
+
+export const getDevice = async (req,res) => {
+    try{
+        const device = await Device.findById(req.params.id);
+        if (!device) return res.status(404).json({message: 'Device not found'});
+            res.status(200).json(device);
+    }catch(error){
+        res.status(500).json({error:error.message});
+    }
+}
