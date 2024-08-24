4766521aef088a0e32746dee3df1b4798a08be7e
ALI9ISSA9 <ali7333170@gmail.com>
2024-08-24 17:29:50 +0300

    created the 'getRoom' operation for the Rooms collection

--- src/controllers/roomsController.js
+++ src/controllers/roomsController.js
@@ -1,20 +1,28 @@
 import { Room } from "../models/roomsModel.js";
 
 export const createRoom = async (req, res) => {
-    try {
-      const { room_id, roomName, devices } = req.body;
-      const newRoom = new Room({
-        room_id,
-        roomName,
-        devices,
-      });
-  
-      await newRoom.save();
-      res.status(201).json(newRoom);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  };
+  try {
+    const { room_id, roomName, devices } = req.body;
+    const newRoom = new Room({
+      room_id,
+      roomName,
+      devices,
+    });
+
+    await newRoom.save();
+    res.status(201).json(newRoom);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 
-  
+export const getRoom = async (req, res) => {
+  try {
+    const room = await Room.findById(req.params.id);
+    if (!room) return res.status(404).json({ message: "Room not found" });
+    res.status(200).json(room);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
