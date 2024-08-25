e9f743927367830de896f30c97bafd3ab91a2513
ALI9ISSA9 <ali7333170@gmail.com>
2024-08-25 15:49:26 +0300

    created the 'getAiSuggestion' operation for the aisuggestion collection

--- src/controllers/AiSuggestionsController.js
+++ src/controllers/AiSuggestionsController.js
@@ -14,3 +14,14 @@ export const createAiSuggestion = async (req, res) => {
       res.status(500).json({ error: error.message });
     }
   };
+
+
+export const getAiSuggestion = async (req, res) => {
+    try {
+      const aiSuggestion = await AiSuggestion.findById(req.params.id);
+      if (!aiSuggestion) return res.status(404).json({ message: 'AI Suggestion not found' });
+      res.status(200).json(aiSuggestion);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
