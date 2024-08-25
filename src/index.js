28683b5ca479cce5acb2624cda8e4d361c9f7f25
ALI9ISSA9 <ali7333170@gmail.com>
2024-08-26 02:12:42 +0300

    added the room routes to the index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import usersRoutes from "./routes/usersRoutes.js";
 import devicesRoutes from "./routes/devicesRoutes.js";
+import roomsRoutes from "./routes/roomsRoutes.js"
 import AISuggestionsRoutes from "./routes/AiSuggestionsRoutes.js";
 import connectToDatabase from "./database/connection.js";
 
@@ -13,6 +14,7 @@ app.use(express.json());
 
 app.use("/users", usersRoutes);
 app.use('/devices',devicesRoutes);
+app.use('/rooms',roomsRoutes);
 app.use('/aisuggestion',AISuggestionsRoutes);
 
 app.listen(8080, () => {
