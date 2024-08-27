f6f9b46ae1872368f2ea7a2c58ba6b8d0b47ea33
ALI9ISSA9 <ali7333170@gmail.com>
2024-08-27 18:54:06 +0300

    modified the routes in userRoutes and protected it by authmiddleware

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,3 +1,23 @@
+// import { Router } from "express";
+// import {
+//     createUser,
+//     updateUser,
+//     getUsers,
+//     getAllUsers,
+//     deleteUser,
+// } from "../controllers/usersController.js";
+
+// const router = new Router();
+
+// router.post("/", createUser);
+// router.get("/:id", getUsers);
+// router.get("/", getAllUsers);
+// router.put("/:id", updateUser);
+// router.delete("/:id", deleteUser);
+
+// export default router;
+
+
 import { Router } from "express";
 import {
     createUser,
@@ -6,13 +26,15 @@ import {
     getAllUsers,
     deleteUser,
 } from "../controllers/usersController.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = new Router();
 
-router.post("/", createUser);
-router.get("/:id", getUsers);
-router.get("/", getAllUsers);
-router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
+// Protect routes that require authentication
+router.post("/", authMiddleware, createUser);
+router.get("/:id", authMiddleware, getUsers);
+router.get("/", authMiddleware, getAllUsers);
+router.put("/:id", authMiddleware, updateUser);
+router.delete("/:id", authMiddleware, deleteUser);
 
 export default router;
