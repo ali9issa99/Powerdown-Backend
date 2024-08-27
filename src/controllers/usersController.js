ee7199a910bc64f0833c85aca74963767d09696c
ALI9ISSA9 <ali7333170@gmail.com>
2024-08-27 18:57:40 +0300

    created protected getAllUsers function in the userController

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -115,3 +115,11 @@ export const getUsers = async (req, res) => {
   }
 };
 
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
