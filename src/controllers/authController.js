b9ba6b743635479e4ebb9a7f81b82e3c542791b2
ALI9ISSA9 <ali7333170@gmail.com>
2024-08-27 18:31:26 +0300

    created the login function inside of the auth controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -35,3 +35,25 @@ export const registerUser = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+
+export const authUser = async (req, res) => {
+    const { email, password } = req.body;
+  
+    try {
+      const user = await User.findOne({ email });
+  
+      if (user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          token: generateToken(user._id),
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid email or password' });
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
