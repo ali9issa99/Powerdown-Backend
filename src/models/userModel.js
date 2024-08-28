7f5fb5fec08c78193c3306e09ace3c6fbfb33f88
ALI9ISSA9 <ali7333170@gmail.com>
2024-08-28 21:52:40 +0300

    adding the passwordhash and password matching functions to the model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,19 +1,56 @@
-import mongoose, { Schema } from "mongoose";
+// import mongoose, { Schema } from "mongoose";
 
-const userschema = new Schema({
+// const userschema = new Schema({
+//     name: {
+//         required: true,
+//         type: String,
+//     },
+//     email: {
+//         required: true,
+//         type:String,
+//         unique:true
+//     },
+//     password: {
+//         required: true,
+//         type:String
+//     }
+// });
+
+// export const User = mongoose.model("User", userschema)
+
+
+import mongoose, { Schema } from "mongoose";
+import bcrypt from 'bcryptjs';
+
+const userSchema = new Schema({
     name: {
-        required: true,
         type: String,
+        required: true,
     },
     email: {
+        type: String,
         required: true,
-        type:String,
-        unique:true
+        unique: true,
     },
     password: {
+        type: String,
         required: true,
-        type:String
     }
+}, { timestamps: true });
+
+// Hash password before saving the user model
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
 });
 
-export const User = mongoose.model("User", userschema)
+// Method to match password during login
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+export const User = mongoose.model("User", userSchema);
