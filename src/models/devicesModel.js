b593b3f51435254f684cbf8dd7a7a3810e4bda96
ALI9ISSA9 <ali7333170@gmail.com>
2024-08-29 02:48:11 +0300

    modified the device model to embed it inside the rooms model

--- src/models/devicesModel.js
+++ src/models/devicesModel.js
@@ -1,20 +1,22 @@
 import mongoose, { Schema } from "mongoose";
+import { consumptionSchema } from "./consumptionModel.js";
 
-const deviceSchema = new Schema({
+export const deviceSchema = new Schema({
     device_id: { 
         type: String,
         required: true,
         unique: true 
-        },
+    },
     deviceName: { 
         type: String,
         required: true 
-        },
+    },
     status: { 
         type: String, 
         enum: ["on", "off"],
         required: true
-        },
-  });
+    },
+    consumptions: [consumptionSchema] // Embedding Consumption // Embedding Consumption
+});
 
 export const Device = mongoose.model('Device', deviceSchema);
