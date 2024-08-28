24e13fe0104788e10517330d7f6ce122ae3b0f37
ALI9ISSA9 <ali7333170@gmail.com>
2024-08-28 21:48:18 +0300

    added the authRoutes to the index.js file

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import roomsRoutes from "./routes/roomsRoutes.js"
 import AISuggestionsRoutes from "./routes/AiSuggestionsRoutes.js";
 import analyticsRoutes  from "./routes/analyticsRoutes.js";
 import consumptionRoutes from "./routes/consumptionRoutes.js"
+import authRoutes from "./routes/authRoutes.js"; 
 import connectToDatabase from "./database/connection.js";
 
 
@@ -15,6 +16,7 @@ const app = express();
 
 app.use(express.json());
 
+app.use("/auth", authRoutes); 
 app.use("/users", usersRoutes);
 app.use('/devices',devicesRoutes);
 app.use('/rooms',roomsRoutes);
