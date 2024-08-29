173d2163d66dc89273a94a4545a686dbef3dd427
ALI9ISSA9 <ali7333170@gmail.com>
2024-08-30 02:39:45 +0300

    added ai suggestions object to the userModel

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,45 +1,10 @@
-// import mongoose, { Schema } from "mongoose";
-// import bcrypt from 'bcryptjs';
-
-// const userSchema = new Schema({
-//     name: {
-//         type: String,
-//         required: true,
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//         unique: true,
-//     },
-//     password: {
-//         type: String,
-//         required: true,
-//     }
-// }, { timestamps: true });
-
-// // Hash password before saving the user model
-// userSchema.pre('save', async function (next) {
-//     if (!this.isModified('password')) {
-//         next();
-//     }
-//     const salt = await bcrypt.genSalt(10);
-//     this.password = await bcrypt.hash(this.password, salt);
-//     next();
-// });
-
-// // Method to match password during login
-// userSchema.methods.matchPassword = async function (enteredPassword) {
-//     return await bcrypt.compare(enteredPassword, this.password);
-// };
-
-// export const User = mongoose.model("User", userSchema);
-
 import mongoose, { Schema } from "mongoose";
 import bcrypt from 'bcryptjs';
 import { roomSchema } from './roomsModel.js'; // Import the Room schema
 import { analyticsSchema } from './analyticsModel.js'; // Import the Analytics schema
+import { aiSuggestionSchema } from './AiSuggestionsModel.js'; // Import the AiSuggestion schema
 
-// User schema with embedded Rooms and Analytics
+// User schema with embedded Rooms, Analytics, and AiSuggestions
 const userSchema = new Schema({
     name: {
         type: String,
@@ -59,7 +24,10 @@ const userSchema = new Schema({
     ], // Embedding Room schema
     analytics: [
         analyticsSchema
-    ] // Embedding Analytics schema
+    ], // Embedding Analytics schema
+    aiSuggestions: [
+        aiSuggestionSchema
+    ] // Embedding AiSuggestion schema
 }, { timestamps: true });
 
 // Hash password before saving the user model
