ef478c232a88124b24c89e621aa0227c3640ef6f
ALI9ISSA9 <ali7333170@gmail.com>
2024-08-29 03:07:55 +0300

    embedded the roomSchema in to the usermodel

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,26 +1,43 @@
 // import mongoose, { Schema } from "mongoose";
+// import bcrypt from 'bcryptjs';
 
-// const userschema = new Schema({
+// const userSchema = new Schema({
 //     name: {
-//         required: true,
 //         type: String,
+//         required: true,
 //     },
 //     email: {
+//         type: String,
 //         required: true,
-//         type:String,
-//         unique:true
+//         unique: true,
 //     },
 //     password: {
+//         type: String,
 //         required: true,
-//         type:String
 //     }
+// }, { timestamps: true });
+
+// // Hash password before saving the user model
+// userSchema.pre('save', async function (next) {
+//     if (!this.isModified('password')) {
+//         next();
+//     }
+//     const salt = await bcrypt.genSalt(10);
+//     this.password = await bcrypt.hash(this.password, salt);
+//     next();
 // });
 
-// export const User = mongoose.model("User", userschema)
+// // Method to match password during login
+// userSchema.methods.matchPassword = async function (enteredPassword) {
+//     return await bcrypt.compare(enteredPassword, this.password);
+// };
+
+// export const User = mongoose.model("User", userSchema);
 
 
 import mongoose, { Schema } from "mongoose";
 import bcrypt from 'bcryptjs';
+import { roomSchema } from './roomsModel.js'; // Import the Room schema
 
 const userSchema = new Schema({
     name: {
@@ -35,7 +52,10 @@ const userSchema = new Schema({
     password: {
         type: String,
         required: true,
-    }
+    },
+    rooms: [
+        roomSchema
+    ] 
 }, { timestamps: true });
 
 // Hash password before saving the user model
