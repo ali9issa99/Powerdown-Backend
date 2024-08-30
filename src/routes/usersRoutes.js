24639425386a0a632ead287d9d95283852df9f31
ALI9ISSA9 <ali7333170@gmail.com>
2024-08-30 03:14:55 +0300

    removing previously commented routes of the user that does not support authMiddleware

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,23 +1,3 @@
-// import { Router } from "express";
-// import {
-//     createUser,
-//     updateUser,
-//     getUsers,
-//     getAllUsers,
-//     deleteUser,
-// } from "../controllers/usersController.js";
-
-// const router = new Router();
-
-// router.post("/", createUser);
-// router.get("/:id", getUsers);
-// router.get("/", getAllUsers);
-// router.put("/:id", updateUser);
-// router.delete("/:id", deleteUser);
-
-// export default router;
-
-
 import { Router } from "express";
 import {
     createUser,
